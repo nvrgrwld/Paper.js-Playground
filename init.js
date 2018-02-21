359c309576d6bb2595c43c5e8cb62334c6b69e68
gregnetland <greg.netland@realityengineering.com>
2018-02-20 21:55:37 -0800

    making random line

--- init.js
+++ init.js
@@ -3,6 +3,6 @@
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight; 
     
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // var ctx = canvas.getContext("2d");
+    // ctx.fillStyle = "black";
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
--- mypaper.js
+++ mypaper.js
@@ -1,37 +1,44 @@
-	// // Create a Paper.js Path to draw a line into it:
-	// var path = new Path();
-	// // Give the stroke a color
-	// path.strokeColor = 'red';
-	// var start = new Point(100, 100);
-	// // Move to start and draw a line from there
-	// path.moveTo(start);
-	// // Note the plus operator on Point objects.
-	// // PaperScript does that for us, and much more!
-	// path.lineTo(start + [ 100, -50 ]);
 
-	// path.add(new Point(200, 200));
-	// path.add(new Point(300, 200));
-	// path.add(new Point(200, 300));
-	
-	var rect = new Path.Rectangle({
-		point: [0, 0],
-		size: [view.size.width, view.size.height],
-		strokeColor: 'white',
-		selected: true
-	});
-	rect.sendToBack();
-	rect.fillColor = '#000000';
+var lowValue = 10;
+var highValue = 30;
+var screenLeftX = 100;
+var screenRightX = 1100;
+var currentX = 100;
+var currentY = 450;
 
+function onFrame(event){
 
-	var circle = new Path.Circle(view.center - 100, 50);
-	circle.strokeColor = 'green';
-	circle.width = 10;
-	var velocity = new Point(5, -5);
-	var acceleration = new Point(0, 0.2);
+	if(currentX < screenRightX){
+		currentX += getRandom();
+		currentY += getRandom() - 50;
+		console.log(currentX, currentY);
+		path.lineTo(currentX, currentY);
 
-	function onFrame(event){
-		circle.position += velocity;
-		velocity += acceleration;
-
+		//circle
+		var circle = new Path.Circle(new Point(currentX, currentY), 10);
+		circle.strokeColor = 'green';
 	}
+}
+
+
+//circle.position = new Point(currentX, currentY);
+//circle.width = 8;
+
+// Create a Paper.js Path to draw a line into it:
+var path = new Path();
+// Give the stroke a color
+path.strokeColor = 'red';
+var start = new Point(currentX, currentY);
+// Move to start and draw a line from there
+path.moveTo(start);
+// Note the plus operator on Point objects.
+// PaperScript does that for us, and much more!
+//path.lineTo(start + [ 100, -50 ]);
+
+function getRandom(){
+	return Math.floor(Math.random() * 100);
+	//return rand; 
+}
+
+console.log(Math.floor(Math.random() * 100));
 
