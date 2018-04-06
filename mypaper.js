d7813c15301ddf7a18b095d65c21f1c8672e676c
Greg <bigdarkpit@gmail.com>
2018-04-06 15:11:30 -0700

    cleanup

--- mypaper.js
+++ mypaper.js
@@ -1,4 +1,5 @@
 
+<<<<<<< Updated upstream
 var lowValue = 10;
 var highValue = 30;
 var screenLeftX = 100;
@@ -41,4 +42,78 @@ function getRandom(){
 }
 
 console.log(Math.floor(Math.random() * 100));
+=======
+// 	// background
+// 	var rect = new Path.Rectangle({
+// 		point: [0, 0],
+// 		size: [view.size.width, view.size.height],
+// 		strokeColor: 'white',
+// 		selected: true
+// 	});
+// 	rect.sendToBack();
+// 	rect.fillColor = '#000000';
+
+//     // circle dropping
+// 	var circle = new Path.Circle(view.center - 100, 50);
+// 	circle.strokeColor = 'green';
+// 	circle.width = 10;
+// 	var velocity = new Point(5, -5);
+// 	var acceleration = new Point(0, 0.2);
+
+// 	function onFrame(event){
+// 		circle.position += velocity;
+// 		velocity += acceleration;
+
+
+// 		// Loop through the segments of the path:
+// 	// for (var i = 0; i <= amount; i++) {
+// 	// 	var segment = path.segments[i];
+
+// 	// 	// A cylic value between -1 and 1
+// 	// 	var sinus = Math.sin(event.time * 3 + i);
+		
+// 	// 	// Change the y position of the segment point:
+// 	// 	segment.point.y = sinus * height + 100;
+// 	// }
+// 	// // Uncomment the following line and run the script again
+// 	// // to smooth the path:
+// 	//  path.smooth();
+// 	}
+
+// 	// triangle magic
+// 	var myPath = new Path();
+// 	myPath.strokeColor = 'white';
+// 	myPath.add(new Point(40, 90));
+// 	myPath.add(new Point(90, 40));
+// 	myPath.add(new Point(140, 90));
+
+// 	myPath.closed = true;
+
+// 	var triangle = new Path.RegularPolygon(new Point(280, 70), 3, 50);
+// 	triangle.fillColor = '#e9e9ff';
+// 	triangle.selected = true;
+
+// 	// The amount of segment points we want to create:
+// var amount = 5;
+
+// // The maximum height of the wave:
+// var height = 60;
+
+// // Create a new path and style it:
+// var path = new Path({
+// 	// 80% black:
+// 	strokeColor: [0.8],
+// 	strokeWidth: 30,
+// 	strokeCap: 'square'
+// });
+
+// // Add 5 segment points to the path spread out
+// // over the width of the view:
+// for (var i = 0; i <= amount; i++) {
+// 	path.add(new Point(i / amount, 1) * view.size);
+// }
+
+// // Select the path, so we can see how it is constructed:
+// path.selected = true;
+>>>>>>> Stashed changes
 
